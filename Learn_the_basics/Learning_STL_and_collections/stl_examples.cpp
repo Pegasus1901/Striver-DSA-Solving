cdfb1d8f06ec16c1cdb5d9099923616660088f48
Prathmesh Sanjay Shevale <77193532+Pegasus1901@users.noreply.github.com>
2026-04-14 12:07:48 +0530

    Printed vectors using iterators

--- Learn_the_basics/Learning_STL_and_collections/stl_examples.cpp
+++ Learn_the_basics/Learning_STL_and_collections/stl_examples.cpp
@@ -7,28 +7,60 @@ void pairOps(){
     pair<int,int> p;
     p={1,2};
     cout<<p.first<<" " << p.second<<endl;
+    
+    cout<<"-------------"<<endl;
+    
     pair<int,int> p1[]={{1,2},{2,3}};
-    cout<<p1[0].first<<" " << p1[0].second;
+    cout<<p1[0].first<<" " << p1[0].second<<endl;
 }
 
 void iteratorOps(vector<int> v,std::vector<pair<int,int>> vec,vector<int> v2,std::vector<int> v1){
-    
+    cout<<"-------------"<<endl;
+    for(vector<int>::iterator it = v.begin();it!=v.end();it++){
+        cout<<*(it)<<endl;
+    }
+    cout<<"-------------"<<endl;
+    for(auto it = vec.begin();it!=vec.end();it++){
+        cout<<it->first<<endl;
+    }
+    cout<<"-------------"<<endl;
+    for (auto it:v2){
+        cout<< it<<endl;
+    }
+    cout<<"-------------"<<endl;
+    for (auto it:v1){
+        cout<< it<<endl;
+    }
 }
 
 void vectorOps(){
+    
     vector<int> v;
     v.push_back(2);
     v.emplace_back(3);
-    std::vector<pair<int,int>> vec;
+    
+    vector<pair<int,int>> vec;
     vec.push_back({1,2});
     vec.emplace_back(5,6);
+    
     vector<int> v2(5,100);
+    
     std::vector<int> v1(v);
+    
     iteratorOps(v,vec,v2,v1);
+    
+    cout<<"-------------"<<endl;
+    
+    cout<<"-------------"<<endl;
+    
+    cout<<"-------------"<<endl;
+    
+    cout<<"-------------"<<endl;
+    
 }
 
 
 int main(){
-    pairOps();
+    // pairOps();
     vectorOps();
 }
