f60e97efbf19dc1933555475d0adc678ee45ffec
Prathmesh Sanjay Shevale <77193532+Pegasus1901@users.noreply.github.com>
2026-04-16 07:45:06 +0530

    Added LeetCode Solution

--- Learn_the_basics/Know_basic_maths/pallindrome_number.cpp
+++ Learn_the_basics/Know_basic_maths/pallindrome_number.cpp
@@ -1,7 +1,6 @@
 #include<iostream>
 using namespace std;
 
-//Can also design return type with bool
 int pallindromeNumber(int num){
     int temp=0;
     while(num!=0){
@@ -11,7 +10,26 @@ int pallindromeNumber(int num){
     }
     return temp;
 }
-
+//LeetCode Solution
+bool isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        long long temp=0,num=x;
+        while(x!=0){
+            int n=x%10;
+            temp=temp*10+n;
+            if(temp>INT_MAX||temp<INT_MIN){
+                return false;
+            }
+            x=x/10;
+        }
+        
+        if(num==temp){
+            return true;
+        }
+        return false;
+    }
 int main(){
     int input;
     cin>>input;
