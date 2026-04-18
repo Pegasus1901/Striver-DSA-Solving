4bd8a4712ca56e455330e4c79a6e019df0336cb8
Prathmesh Sanjay Shevale <77193532+Pegasus1901@users.noreply.github.com>
2026-04-18 16:03:44 +0530

    Factorial of given number using recursion

---
+++ Learn_the_basics/Learn_basic_recursion/factorial_using_recursion.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+void printN(int n,int s){
+    if(n<1) {
+        cout<<s;
+        return ;
+    }
+    s=s*n;
+    n--;
+    printN(n,s);
+}
+
+int main()
+{
+    int num=0,sum=1;
+    cin>>num;
+    printN(num,sum);
+
+    return 0;
+}
