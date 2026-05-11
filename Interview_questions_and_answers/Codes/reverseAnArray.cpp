91f15c455aafaf079bec62c31be1408af34eddb0
Prathmesh Sanjay Shevale <77193532+Pegasus1901@users.noreply.github.com>
2026-05-11 22:39:39 +0530

    Implement function to reverse an array

--- Interview_questions_and_answers/Codes/reverseAnArray.cpp
+++ Interview_questions_and_answers/Codes/reverseAnArray.cpp
@@ -1,1 +1,22 @@
+#include <iostream>
+using namespace std;
 
+void revArray(int arry[],int s){
+    int temp=0;
+    for(int i=0;i<s/2;i++){
+        temp = arry[i];
+        arry[i]=arry[s-1-i];
+        arry[s-1-i]=temp;
+    }
+    for(int i = 0; i < s; i++){
+        cout<<arry[i]<<" ";
+    }
+}
+
+int main()
+{
+    int arr[] = {1,2,3,4};
+    int s=sizeof(arr)/sizeof(arr[0]);
+    revArray(arr,s);
+    return 0;
+}
