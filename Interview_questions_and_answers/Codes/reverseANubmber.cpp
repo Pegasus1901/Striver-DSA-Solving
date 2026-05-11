d41c666d2aa3a1b42a05b8b502a39af3fbd98cd4
Prathmesh Sanjay Shevale <77193532+Pegasus1901@users.noreply.github.com>
2026-05-11 20:40:30 +0530

    Implement function to reverse a number

--- Interview_questions_and_answers/Codes/reverseANubmber.cpp
+++ Interview_questions_and_answers/Codes/reverseANubmber.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
+using namespace std;
 
+void revNums(int n){
+    int rev=0;
+    while(n>0){
+        rev=rev*10+(n%10);
+        n/=10;
+    }
+    cout<<rev;
+}
+
+int main()
+{
+    int num = 1234;
+    revNums(num);
+    return 0;
+}
