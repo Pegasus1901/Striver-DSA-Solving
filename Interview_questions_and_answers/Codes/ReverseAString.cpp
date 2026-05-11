a6fb038b76193b94407dadb8cdecb830b36ba4ae
Prathmesh Sanjay Shevale <77193532+Pegasus1901@users.noreply.github.com>
2026-05-11 20:28:09 +0530

    Implement string reversal functions in C++
    Added two functions to reverse a string: revString and revStringOptimal. The optimal function uses a two-pointer approach for efficiency.

--- Interview_questions_and_answers/Codes/ReverseAString.cpp
+++ Interview_questions_and_answers/Codes/ReverseAString.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+#include <string>
+using namespace std;
 
+void revString(string s){
+    string rev=s;
+    int n= s.length();
+    for(int i=0;i<n;i++){
+        rev[i]=s[n-1-i];
+    }
+    cout<<rev;
+}
+
+void revStringOptimal(string s){
+    int left=0,right=s.length()-1;
+    
+    while(left<right){
+        char temp = s[left];
+        s[left]=s[right];
+        s[right]=temp;
+        
+        left++;
+        right--;
+    }
+    cout<<s;
+}
+
+int main()
+{
+    string word = "CANDIDATE";
+    // revString(word);
+    revStringOptimal(word);
+    return 0;
+}
